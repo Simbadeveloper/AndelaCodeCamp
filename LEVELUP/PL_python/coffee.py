d2cf7ad17fa4c23f0c4e15b17b7525a8d8754c2e
silas Omurunga <silverdeltamega@gmail.com>
2018-12-27 12:42:56 +0300

    update coffee.py

---
+++ LEVELUP/PL_python/coffee.py
@@ -0,0 +1,36 @@
+"""Instruction for coffee machine
+    1. make and serve me, you and Gibbs a cup of coffee(add coffee, and hot water, stir)
+    2. change how the mix is stirred
+    3. A  better way to make cofee with less repetition
+    4. Make you coffe with milk and suger (add suger, and milk)
+    5. Make Gibbs coffe with milk, sugar and something else (add sugar, milk'...)
+    6. Refactor
+"""
+
+# make my coffee
+ingredients = ['coffee', 'hot water']
+print ('started making coffee...')
+print('Getting cup')
+print('Adding {}'.format(', '.join(ingredients)))
+print('Stir the mix')
+print('Finished making coffee...')
+my_coffee = 'Tasty coffee'
+print("--Here' your {} {}. Enjoy !!!! --\n".format(my_coffee, 'silas'))
+
+#make you coffee
+print ('started making coffee...')
+print('Getting cup')
+print('Adding {}'.format(', '.join(ingredients)))
+print('Stir the mix')
+print('Finished making coffee...')
+your_coffee = 'Tasty coffee'
+print("--Here' your {} {}. Enjoy !!!! --\n".format(your_coffee, 'You'))
+
+#make Gibbs coffee
+print ('started making coffee...')
+print('Getting cup')
+print('Adding {}'.format(', '.join(ingredients)))
+print('Stir the mix')
+print('Finished making coffee...')
+gibbs_coffee = 'Tasty coffee'
+print("--Here' your {} {}. Enjoy !!!! --\n".format(gibbs_coffee, 'Gibbs'))
