270f757d1a52ff93559b40039c285998cd613769
silas Omurunga <silverdeltamega@gmail.com>
2018-12-27 09:27:50 +0300

    add coffe.py, bank_account.py, test_bank_account.py

---
+++ LEVELUP/python_unit_test/test_bank_account.py
@@ -0,0 +1,21 @@
+import unittest
+from bank_account import BankAccount
+
+class AccountBalanceTestCase(unittest.TestCase):
+    def setUp(self):
+        self.account_silas = BankAccount()
+
+    def test_balance(self):
+        self.assertEqual(self.account_silas.balance, 3000, msg = "Account Balance invalid.")
+
+    def test_deposit(self):
+        self.account_silas.deposit(4000)
+        self.assertEqual(self.account_silas.balance, 7000, msg = "Deposit method inaccurates")
+
+    def test_withdraw(self):
+        self.account_silas.withdraw(3000)
+        self.assertEqual(self.account_silas.balance, 0, msg="withdraw method invalid.")
+
+    def test_invalid_transaction(self):
+        self.assertEqual(self.account_silas.withdraw(5000), 'invalid transaction', msg ='invalid transaction')
+
